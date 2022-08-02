507d0e9646bb0afa309577d67a1e3e0185ba02bc
Nabilaagustina <agustinacahyani5@gmail.com>
2022-08-03 01:25:00 +0700

    update

--- pages/01_Percobaan Hashing.py
+++ pages/01_Percobaan Hashing.py
@@ -1,36 +1,26 @@
-<<<<<<< HEAD
 # Imoprt library
-=======
->>>>>>> 4aeb01f8942e64b4526cdb4d2bcecea14df9c693
 import streamlit as st
 import hashlib  # import hashlib module
 
-st.title("Lets Try Hashing!")
-text = st.text_input("Input Your Text")
+st.set_page_config(
+    page_title="Percobaan Hashing",
+    page_icon= "🔏",
+)
 
-<<<<<<< HEAD
 st.title("Let's Try Hashing!")
 st.header("Percobaan Hashing")
 text = st.text_input("Masukkan data: ")
-=======
-st.write('Text yang di Input adalah: ',text)
->>>>>>> 4aeb01f8942e64b4526cdb4d2bcecea14df9c693
 
 st.write('Text yang di Input adalah: ',text)
 # initialize using sha256
-st.header('\nSHA256')
+st.subheader('SHA256')
 # use the whole string at once
-x = hashlib.sha256(text.encode('utf8'))
-st.write(x.digest())
+x = hashlib.sha256(text.encode())
+st.write(x.hexdigest())
 
 # initialize using md5
-<<<<<<< HEAD
 st.subheader('md5')
-=======
-st.header('\nmd5')
-m = hashlib.md5()
-
->>>>>>> 4aeb01f8942e64b4526cdb4d2bcecea14df9c693
 # use the whole string at once
-x = hashlib.md5(text.encode('utf8'))
-st.write(x.digest())
+x = hashlib.md5(text.encode())
+st.write(x.hexdigest())
+st.write('test')
